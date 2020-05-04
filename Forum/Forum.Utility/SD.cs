a37908b2643a566c414e282f829afcab1ec44e69
amyard <maksymturenko@gmail.com>
2020-05-05 01:05:49 +0300

    start app. init db.

---
+++ Forum/Forum.Utility/SD.cs
@@ -0,0 +1,9 @@
+﻿namespace Forum.Utility
+{
+    public class SD
+    {
+        // Users Role
+        public const string Role_Admin = "Admin";
+        public const string Role_User = "Authorized User";
+    }
+}
